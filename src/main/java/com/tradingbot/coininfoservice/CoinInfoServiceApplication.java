e95559a103285603ce297d3e0d7adc5fcb9b7d1f
Frank Park <jehyn923@gmail.com>
2021-10-08 23:46:10 +0900

    fix: eureka 설정

--- src/main/java/com/tradingbot/coininfoservice/CoinInfoServiceApplication.java
+++ src/main/java/com/tradingbot/coininfoservice/CoinInfoServiceApplication.java
@@ -2,6 +2,7 @@ package com.tradingbot.coininfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
